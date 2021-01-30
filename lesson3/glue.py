4da13e45707c0a6a4b053c42257964335ed53cb4
RHK28 <hamid.chayeb97@gmail.com>
2021-01-30 13:12:33 +0200

    homework3

---
+++ lesson3/glue.py
@@ -0,0 +1,6 @@
+
+
+num1 = int(input("enter the nember 1"))
+num2 = int(input("enter the nember 2"))
+num3 = int(input("enter the nember 3"))
+print(num1, num2 , num3)
---
+++ lesson4/month.py
@@ -0,0 +1,18 @@
+
+months = int(input("enter nember from 1 to 12"))
+if months == :
+    print("janury")
+if months = "2":
+    print("februry")
+ if months = "3":
+    print("mart")
+ if months = "4":
+    print("fevral")
+ if months = "5":
+    print("mai")
+ if months = "6":
+    print("june")
+ if months = "7":
+    print("jully")
+ else:
+     print("enter corect nember please")

c135157f932c53fae86d785d1818dd40ed007c3f
RHK28 <hamid.chayeb97@gmail.com>
2021-01-31 02:31:21 +0200

    first commit

--- hard_play.py
+++ hard_play.py
@@ -1,1 +1,7 @@
 
+month = int(input("enter"))
+try:
+    if month == 1:
+        print("janvier")
+except ValueError:
+        print("sorry enter nember from 1 to 12")
--- lesson4/month.py
+++ lesson4/month.py
@@ -1,18 +1,31 @@
 
 months = int(input("enter nember from 1 to 12"))
-if months == :
-    print("janury")
-if months = "2":
-    print("februry")
- if months = "3":
-    print("mart")
- if months = "4":
-    print("fevral")
- if months = "5":
-    print("mai")
- if months = "6":
-    print("june")
- if months = "7":
-    print("jully")
- else:
-     print("enter corect nember please")
+try:
+   if months == 1 :
+      print("January")
+   if months == 2:
+      print("february")
+   if months == 3:
+      print("march")
+   if months == 4:
+      print("april")
+   if months == 5:
+      print("may")
+   if months == 6:
+      print(" june")
+   if months == 7:
+      print("julie")
+   if months == 8:
+      print("August")
+   if months == 9:
+      print("September")
+   if months == 10:
+      print("Octeber")
+   if months == 11:
+      print("November")
+   if months == 12:
+     print("December")
+except Exception:
+    print("enter nember from 1 to 12 please ))")
+#else:
+   # print("enter nember from 1 to 12 please ))")

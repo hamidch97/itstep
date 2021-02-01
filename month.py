a6717f298960fbe25b5c8703cd98378e9180875d
hamidch97 <hamid.rhk97@gmail.com>
2021-02-01 16:47:25 +0200

    homework4

---
+++ month.py
@@ -0,0 +1,31 @@
+try:
+    months = int(input("enter nember from 1 to 12"))
+
+    if months == 1:
+        print("January")
+    if months == 2:
+        print("february")
+    if months == 3:
+        print("march")
+    if months == 4:
+        print("april")
+    if months == 5:
+        print("may")
+    if months == 6:
+        print(" june")
+    if months == 7:
+        print("julie")
+    if months == 8:
+        print("August")
+    if months == 9:
+        print("September")
+    if months == 10:
+        print("Octeber")
+    if months == 11:
+        print("November")
+    if months == 12:
+        print("December")
+except Exception:
+    print("enter nember from 1 to 12 please ))")
+else:
+    print("enter nember from 1 to 12 please ))")

c157923f83bd4094d4b05f7b058f8c16cf5e1c0e
hamidch97 <hamid.rhk97@gmail.com>
2021-02-01 16:41:01 +0200

    homework4

---
+++ lesson4/extra_exercise.py
@@ -0,0 +1,12 @@
+try:
+    # n is a nember from1 to 12
+    n = int(input("enter nember from1 to 12"))
+    # s is a list months
+    s = ['janaury', 'febraury', 'march', 'april', 'may', 'june', 'julie', 'august', 'september', 'october', 'november',
+         'december']
+    if n == int:
+        print(s[n - 1])
+except Exception:
+    print("enter integer nember from 1 to 12 please")
+else:
+    print("enter integer nember from 1 to 12 please")
